8a43afc9292377de082e420787c289baf927bda9
Jack New <njeka0108@gmail.com>
2023-08-31 17:44:49 +0300

    add info ext

--- src/info/include/info/version.h
+++ src/info/include/info/version.h
@@ -12,6 +12,13 @@ constexpr std::string_view info_project_revision_datetime() { return PROJECT_REV
 constexpr std::string_view info_project_revision_history() { return PROJECT_REVISION_HISTORY; }
 constexpr std::string_view info_project_version() { return PROJECT_VERSION; }
 
+constexpr std::string_view info_compiler_name() { return COMPILER_NAME; }
+constexpr std::string_view info_compiler_version() { return COMPILER_VERSION; }
+constexpr std::string_view info_host_system() { return HOST_SYSTEM; }
+
+constexpr std::string_view info_boost_version() { return BOOST_VERSION; }
+constexpr std::string_view info_boost_libraries() { return BOOST_LIBRARIES; }
+
 std::string info_project_full();
 
 }  // namespace info
--- src/info/src/version.cpp
+++ src/info/src/version.cpp
@@ -6,8 +6,14 @@ std::string info::info_project_full() {
   std::stringstream ss;
   ss << "author: " << info_project_author() << std::endl;
   ss << "url: " << info_project_repository_url() << std::endl;
+  ss << "version: " << info_project_version() << " (revision:" << info_project_revision() << ")" << std::endl;
+
+  ss << "compiler: " << info_compiler_name() << " " << info_compiler_version() << std::endl;
+  ss << "platform: " << info_host_system() << std::endl;
+  ss << "using:\n"
+     << "\tBoost version " << info_boost_version() << "\n\t\tlibraries: " << info_boost_libraries() << std::endl;
+
   ss << "branch: " << info_project_repository_branch() << std::endl;
-  ss << "rev: " << info_project_revision() << std::endl;
   ss << "rev date: " << info_project_revision_datetime() << std::endl;
   ss << "rev hist: " << info_project_revision_history() << std::endl;
   return ss.str();
